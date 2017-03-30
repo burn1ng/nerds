a5d1ae1ed20dcf55c96202253e2fcc29dd2d8aa7
Andrei Dushkou <andrew.burn1ng@gmail.com>
2017-03-30 03:17:51 +0300

    rangeFilter added in shop

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@
 
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
-
+//= ../../bower_components/ion.rangeSlider/js/ion.rangeSlider.js
 
 /*
  * Custom
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,43 @@
- $(document).ready(function(){
-        $('.carousel').carousel('pause');
-    });
+$(document).ready(function(){
+
+	// range slider
+	var $range = $("#range-input");
+	var $rangeFromField = $("#rangeFrom");
+	var $rangeToField = $("#rangeTo");
+
+	$range.ionRangeSlider({
+		type: "double",
+		grid: false,
+		hide_min_max: true,
+		hide_from_to: true,
+	    min: 0,
+	    max: 25000,
+	    from: 0,
+	    to: 15000
+	});
+
+	var $rangeData = $range.data("ionRangeSlider");
+
+
+	$range.on("change", function() {
+		var $this = $(this),
+			value = $this.prop("value").split(";");
+		$rangeFromField.val(value[0]);
+		$rangeToField.val(value[1]);
+	});
+
+	$rangeFromField.on("change", function() {
+		var $this = $(this);
+		$rangeData.update({
+			from: $this.val()
+		});
+	});
+
+	$rangeToField.on("change", function() {
+		var $this = $(this);
+		$rangeData.update({
+			to: $this.val()
+		});
+	});
+
+});
