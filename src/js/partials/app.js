cac064f88f95ef58518a2bb9aed03fd572b7366d
Andrei Dushkou <andrew.burn1ng@gmail.com>
2017-04-01 04:43:04 +0300

    main layout finished

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,63 @@
+// var googleMapScript = document.createElement("script");
+
+// googleMapScript.src = "https://maps.googleapis.com/maps/api/js";
+// //googleMapScript.async = true; //asynchronous
+// document.getElementsByTagName("head")[0].appendChild(googleMapScript);
+
+google.maps.event.addDomListener(window, 'load', init);
+        
+function init() {
+    var mapOptions = {
+        zoom: 17,
+        scrollwheel: false,
+        center: new google.maps.LatLng(53.9492715,27.6808287),
+        styles: 
+        	[{"featureType":"landscape",
+        	"elementType":"labels",
+        	"stylers":[{"visibility":"off"}]},
+
+        	{"featureType":"transit",
+        	"elementType":"labels",
+        	"stylers":[{"visibility":"off"}]},
+
+        	{"featureType":"poi",
+        	"elementType":"labels",
+        	"stylers":[{"visibility":"off"}]},
+
+        	{"featureType":"water",
+        	"elementType":"labels",
+        	"stylers":[{"visibility":"off"}]},
+
+        	{"featureType":"road",
+        	"elementType":"labels.icon",
+        	"stylers":[{"visibility":"off"}]},
+
+        	{"stylers":
+        	[{"hue":"#00aaff"},
+        	{"saturation":-100},
+        	{"gamma":2.15},
+        	{"lightness":12}]},
+
+        	{"featureType":"road",
+        	"elementType":"labels.text.fill",
+        	"stylers":[{"visibility":"on"},{"lightness":24}]},
+
+        	{"featureType":"road",
+        	"elementType":"geometry",
+        	"stylers":[{"lightness":57}]}]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+ 	var gimage = 'img/map-baloon.png';
+	
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(53.948415,27.682158),
+        map: map,
+		icon: gimage,
+        title: 'Тут наш офис'					
+    });
+}
+
 $(document).ready(function(){
 
 	// range slider
@@ -64,6 +124,41 @@ $(document).ready(function(){
 		$this.val( function() {
 			return ( input === 0 ) ? "" : input.toLocaleString( "ru-RU" );
 		});
+	});
+
+
+	$(".shop-pagination-box:first").next().addClass('active');  		//default active- 1st page
+
+	$('.shop-pagination').find('.shop-pagination-box:not(:first):not(:last)').click(function(e) {
+			e.preventDefault();
+			$(this).parent().children().removeClass('active');
+			$(this).addClass('active');
+	});
+
+	$(".btn-next").click(function(e) {
+		e.preventDefault();
+		var activeItem = $(".shop-pagination-box.active");
+		var nextItem = activeItem.next();
+		activeItem.removeClass("active");
+
+		if ( nextItem.next().length == 0 || nextItem.length == 0 ) {
+			nextItem = $(".shop-pagination-box:first").next();
+		}
+
+		nextItem.addClass("active");
+	});
+
+	$(".btn-prev").click(function(e) {
+		e.preventDefault();
+		var activeItem = $(".shop-pagination-box.active");
+		var prevItem = activeItem.prev();
+		activeItem.removeClass("active");
+
+		if ( prevItem.prev().length == 0 || prevItem.length == 0 ) {
+			prevItem = $(".shop-pagination-box:last").prev();
+		}
+
+		prevItem.addClass("active");
 	});
 
 
